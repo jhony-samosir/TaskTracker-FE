1fdaba85914523ab90ad902bf4b28cd1b5216a95
jhony-samosir <filterch07@gmail.com>
2026-06-01 21:03:06 +0700

    feat: add @angular/animations dependency and update app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,5 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimationsAsync(),
+    provideNativeDateAdapter(),
   ],
 };
--- src/app/features/task-management/components/task-form-dialog/task-form-dialog.ts
+++ src/app/features/task-management/components/task-form-dialog/task-form-dialog.ts
@@ -8,7 +8,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -38,7 +37,6 @@ function assigneeNotReviewerValidator(control: AbstractControl): ValidationError
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatNativeDateModule,
     MatProgressSpinnerModule,
     MatSelectModule,
   ],
